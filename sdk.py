81d4372242a3af49de317ea0741caee51ab744c7
Zaphkiel1031 <68491362+Zaphkiel1031@users.noreply.github.com>
2024-06-05 14:26:54 +0000

    你的提交消息

--- sdk.py
+++ sdk.py
@@ -3,11 +3,11 @@ import streamlit.components.v1 as components
 
 st.title("Coze Bot Integration")
 
-# 显示GIF图片
+
 gif_url = "https://raw.githubusercontent.com/j7808833/test_02/main/pic/Cyberpunk_bar_05.gif"
 st.image(gif_url)
 
-# 嵌入HTML和JavaScript代码
+
 html_code = """
 <!DOCTYPE html>
 <html>
@@ -22,11 +22,11 @@ html_code = """
       try {
         new CozeWebSDK.WebChatClient({
           config: {
-            bot_id: '7376255263235424257',  // 替换为你的Bot ID
+            bot_id: '7376255263235424257',  
           },
           componentProps: {
-            title: 'Coze',  // 聊天组件的标题
-            lang: 'zh-TW'  // 设置语言为繁体中文
+            title: 'Coze',  
+            lang: 'zh-TW'  
           },
         });
         console.log("Coze Web SDK initialized successfully.");
@@ -40,5 +40,5 @@ html_code = """
 </html>
 """
 
-# 使用Streamlit的HTML组件嵌入代码
+
 components.html(html_code, height=600)
